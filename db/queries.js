436c0463c2658d7557a5094c12e59dfbf776361d
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2024-12-05 13:54:31 -0500

    Implement CRUD queries for groceries table

--- db/queries.js
+++ db/queries.js
@@ -33,6 +33,50 @@ async function deleteAllCategories() {
   await pool.query("DELETE FROM categories");
 }
 
+// Implement postgreSQL queries for groceries table
+
+async function getAllGroceries() {
+  const { rows } = await pool.query("SELECT * FROM groceries");
+  return rows;
+}
+
+async function getAllGroceriesUnderCategory(categ_id) {
+  const { rows } = await pool.query(
+    "SELECT * FROM groceries WHERE category_id = $1",
+    [categ_id]
+  );
+  return rows;
+}
+
+async function getSpecificGrocery(grocery_id) {
+  const { rows } = await pool.query("SELECT * FROM groceries WHERE id = $1", [
+    grocery_id,
+  ]);
+  return rows;
+}
+
+async function createGrocery(name, price, stock, categoryId) {
+  await pool.query(
+    "INSERT INTO groceries(name, price, stock, categoryId) VALUES($1, $2, $3, $4)",
+    [name, price, stock, categoryId]
+  );
+}
+
+async function updateGrocery(grocery_id, name, price, stock, categoryId) {
+  await pool.query(
+    "UPDATE groceries SET name = $1, price = $2, stock = $3, categoryId = $4 WHERE groceryId = $5",
+    [name, price, stock, categoryId, grocery_id]
+  );
+}
+
+async function deleteSpecificGrocery(grocery_id) {
+  await pool.query("DELETE FROM groceries WHERE id = $1", [grocery_id]);
+}
+
+async function deleteAllGroceriesUnderCategory(categ_id) {
+  await pool.query("DELETE FROM groceries WHERE category_id = $1", [categ_id]);
+}
+
 module.exports = {
   getAllCategories,
 };
