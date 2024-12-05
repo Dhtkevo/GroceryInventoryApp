cada87304e41ce3944a1a54b8d175f6c480eaada
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2024-12-05 14:08:48 -0500

    Implement express error handling middleware

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const indexRouter = require("./routes/indexRouter");
 const app = express();
 
 app.use("/", indexRouter);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
