082d9c7e06fcb41bf733bbd0ba80d88ef000ee32
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2024-12-11 18:25:30 -0500

    Implement update grocery validation backend

--- controllers/groceriesController.js
+++ controllers/groceriesController.js
@@ -1,5 +1,24 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const groceryError = "One or more grcery fields contain errors";
+
+const validateGroceryFields = [
+  body("groceryName")
+    .trim()
+    .notEmpty()
+    .isAlpha("en-US", { ignore: " " })
+    .withMessage(`name - ${groceryError}`),
+  body("groceryPrice")
+    .notEmpty()
+    .isDecimal()
+    .withMessage(`price - ${groceryError}`),
+  body("groceryStock")
+    .notEmpty()
+    .isInt()
+    .withMessage(`stock - ${groceryError}`),
+];
 
 exports.getGroceryInformation = asyncHandler(async (req, res) => {
   const grocery = await db.getSpecificGrocery(Number(req.params.groceryId));
@@ -14,21 +33,30 @@ exports.getUpdateGrocery = asyncHandler(async (req, res) => {
   res.render("updateGrocery", { grocery });
 });
 
-exports.postUpdateGrocery = asyncHandler(async (req, res) => {
-  const { groceryId } = req.params;
-  const { groceryName } = req.body;
-  const { groceryPrice } = req.body;
-  const { groceryStock } = req.body;
-  const grocery = await db.getSpecificGrocery(Number(groceryId));
-  await db.updateGrocery(
-    Number(groceryId),
-    groceryName,
-    Number(groceryPrice),
-    Number(groceryStock),
-    grocery[0].category_id
-  );
-  res.redirect("/groceries/" + groceryId);
-});
+exports.postUpdateGrocery = [
+  validateGroceryFields,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).send(groceryError);
+    }
+
+    const { groceryId } = req.params;
+    const { groceryName } = req.body;
+    const { groceryPrice } = req.body;
+    const { groceryStock } = req.body;
+    const grocery = await db.getSpecificGrocery(Number(groceryId));
+    await db.updateGrocery(
+      Number(groceryId),
+      groceryName,
+      Number(groceryPrice),
+      Number(groceryStock),
+      grocery[0].category_id
+    );
+    res.redirect("/groceries/" + groceryId);
+  }),
+];
 
 exports.getDeleteGrocery = asyncHandler(async (req, res) => {
   const { groceryId } = req.params;
